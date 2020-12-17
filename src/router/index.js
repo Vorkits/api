d18f2112a64cea65732c569380ee98c6afff4e57
Александр Рыбкин <@new.local.itstep>
2020-12-17 17:42:13 +0600

    ss

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,49 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Tournaments from '../views/Tournaments.vue'
+import Players from '../views/Players.vue'
+import Matches from '../views/Matches.vue'
+import About from '../views/About.vue'
+import Smash from '../views/Smash.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
+    name: 'home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/tournaments',
+    name: 'tournaments',
+    component: Tournaments
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    //component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/players',
+    name: 'players',
+    component: Players
+  },
+  {
+    path: '/matches',
+    name: 'matches',
+    component: Matches
+  },
+  {
+    path: '/smash',
+    name: 'smash',
+    component: Smash
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: About
+  },
 ]
 
 const router = new VueRouter({
--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,15 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    companyName:'Company Name',
+    levels: [
+      {name:'Total Beginner', abbr: 'T', color:'#467E32'}, {name: 'Beginner', abbr: 'B', color:'#8BC34A'}, 
+      {name:'Intermediate', abbr: 'I', color:'#F6C02D'}, {name: 'Advanced', abbr: 'A', color:'#EA7A00'},
+      {name:'Pro', abbr: 'P', color:'#BF3B0C'}
+    ],
+    info: [
+      {category: 'Tennis Players', val: '32.111'},{category: 'Tennis Players Chat', val: '277.380'},{category: 'Scheduled Matches', val: '105.781'}
+    ]
   },
   mutations: {
   },
