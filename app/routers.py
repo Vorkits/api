7aec8450e0bc9125a0502c65fee5b7bed1270f0b
Vorkits <reazer381@gmail.com>
2021-01-20 17:27:38 +0600

    dsa

--- app/routers.py
+++ app/routers.py
@@ -3,7 +3,10 @@ from .login_r import login_route
 from .user_r import user_route
 from .courts_r import court
 from .games_r import game_r
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app) # This will enable CORS for all routes
+
 app.register_blueprint(login_route, url_prefix='/api/auth')
 app.register_blueprint(user_route, url_prefix='/api/user')
 app.register_blueprint(court, url_prefix='/api/court')
