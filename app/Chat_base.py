725b54aa13d7a6b02354aabdf73448bdc2bdab14
Vorkits <reazer381@gmail.com>
2021-02-03 00:37:24 +0600

    dsadas

---
+++ app/Chat_base.py
@@ -0,0 +1,20 @@
+from app.firebase_init import Firebase
+import uuid
+import app.firebase as fr
+from hashlib import sha256
+
+class Chat_base(Firebase):
+    def create_chat(self,id):
+        db=self.db
+        db.child('chats').child(id).set({})
+        return {'status':'success'}
+    def get_chat(self,id):
+        db=self.db
+        data=db.child('chats').child(id).get().val()
+        return {'status':'success','data':dict(data),'hex':sha256(str(dict(data)).encode()).hexdigest()}
+    def send_message(self,id,message,owner):
+        db=self.db
+        message_id=uuid.uuid4().hex
+        message_data={'owner':owner,'message':message}
+        data=db.child('chats').child(id).child(message_id).set(message_data)
+        return {'status':'success'}
--- app/test.py
+++ app/test.py
@@ -1,13 +1,2 @@
-
-array=[[1,22,34,543],[2,34,45,43],[1,3,45,54]] #EXAMPLE
-S=int(input())
-R=int(input())
-for i in array:
-      indexsum=0
-      elementsum=0
-      for j in i:
-            indexsum+=j
-            elementsum+=i[j]
-      ostatok=elementsum% R
-      if (indexsum < S) and (ostatok %2==0):
-            print(array[i])
+from hashlib import sha256
+print(sha256('ss'.encode()).hexdigest())
