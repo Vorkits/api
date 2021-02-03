a8925b828d4ab5821ce9fe0bfdad34c617586905
Vorkits <reazer381@gmail.com>
2021-02-03 22:53:31 +0600

    вывы

--- app/Chat_base.py
+++ app/Chat_base.py
@@ -2,7 +2,7 @@ from app.firebase_init import Firebase
 import uuid
 import app.firebase as fr
 from hashlib import sha256
-
+import time 
 class Chat_base(Firebase):
     def create_chat(self,id):
         db=self.db
@@ -10,13 +10,15 @@ class Chat_base(Firebase):
         return {'status':'success'}
     def get_chat(self,id):
         db=self.db
-        data=db.child('chats').child(id).get().val()
+        data=db.child('chats').child(id).order_by_key().get().val()
         if not data:
             data={}
         return {'status':'success','data':dict(data),'hex':sha256(str(dict(data)).encode()).hexdigest()}
     def send_message(self,id,message,owner):
         db=self.db
-        message_id=uuid.uuid4().hex
+        id=uuid.uuid4().hex
+        ts = str(int(time.time()))
+        message_id=ts+id
         data=dict(db.child('users').child(owner).get().val())
         message_data={'owner':owner,'message':message,'name':data['name'],'photo':data['photo']}
         db.child('chats').child(id).child(message_id).set(message_data)
