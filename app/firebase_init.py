46af993e76da3e382224e20a7cf185676dc5332b
Vorkits <reazer381@gmail.com>
2021-02-07 00:20:36 +0600

    dadsa

--- app/firebase_init.py
+++ app/firebase_init.py
@@ -2,7 +2,17 @@ import pyrebase
 import uuid
 import os
 import app.firebase as fr
-
+config = {
+    "apiKey": "AIzaSyDYsYC0LnriZt1JxLKpFKV0HIfw2slT1ac",
+    "authDomain": "orac-9d788.firebaseapp.com",
+    "projectId": "orac-9d788",
+    "storageBucket": "orac-9d788.appspot.com",
+    "messagingSenderId": "364935870344",
+    "appId": "1:364935870344:web:eacf38c9a8f1f53d8eac3d",
+   "measurementId": "G-Y9LZYCKM4Y",
+   "databaseURL":'https://orac-9d788-default-rtdb.firebaseio.com/',
+   "serviceAccount": "orac-9d788-firebase-adminsdk-2iqe3-d7e78188c5.json"
+  }
 firebase = pyrebase.initialize_app(config)
 
 class Firebase:
